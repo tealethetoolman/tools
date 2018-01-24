082a7d2d4a5ee18dbcca190979969353f031ed14
ME <me@clearbox>
2018-01-23 18:51:27 -0600

    opens looks like a working menu. I'm going through and changing print all the prints for ouut

--- modules/display.pm
+++ modules/display.pm
@@ -2,7 +2,7 @@ package modules::display;
 use Exporter 'import';
 use Data::Dumper;
 &ouut( source => 'display', message => "loading module", tab => 1, severity => 'debug' );
-@EXPORT = qw(ouut ouut_string ouut_menu ouut_line ouut_clear ouut_quest);
+@EXPORT = qw(ouut ouut_string ouut_menu ouut_line ouut_clear ouut_title ouut_quest);
 $main::data{menu}{main}{display}	=	{
 	option => "d",
 	name => "DISPLAY",
@@ -48,9 +48,30 @@ sub ouut	{
 sub ouut_clear	{
 	return system("clear");
 }
-
+sub ouut_title	{	
+	my $screen_width = 78; # this isn't the actual width, just the width that I made in ouut_line
+	my $input = shift;
+	print "input => $input \n";
+	$input =~ s/ /-/g; # replace spaces with -
+	my $length = length($input);
+	print "length = $length\n";
+	my $filler_size = $screen_width - $input;
+	print "screen: $screen_width math ". ($screen_width - $length ). " end \n";
+	print "filler size: $filler_size Length: $length Screen WIdth $screen_width.....\n";
+	my $line = "|-"; # ip put a single hyphen in there so that i have an equal length on both the front and back
+#	$line .= "-" if (($filler_size %% 2 == 1) and ($filler_size --) );
+	for (1..($filler_size/2))	{
+		$line .= "-";
+	}
+	$line .= "$input";
+	for (1..($filler_size/2))	{
+		$line .= "-";
+	}
+	$line .= "[]\n";
+	print $line;
+}
 sub ouut_line	{
-	print "|[]----------------------------------------------------------------------------[]\n";
+	print "|[]-----------------------------------------------------------------------------[]\n";
 }
 
 sub ouut_quest	{
@@ -69,12 +90,9 @@ sub ouut_menu 	{
 	my $which_menu = shift;
 	my $output_line;
 	for (keys $main::data{menu}{$which_menu})	{
+		ouut_title("MAIN MENU");
 		my $menu_item = $_;
 		print "-> ".$main::data{menu}{$which_menu}{$menu_item}{option}." : ".$main::data{menu}{$which_menu}{$menu_item}{name}."\n";
-#		for(keys $main::data{menu}{$which_menu}{$menu_item})	{
-#			my $menu_item_option = $_;
-#			print "Menu: $which_menu Item: $menu_item Option: $menu_item_option Value: ".$main::data{menu}{$which_menu}{$menu_item}{$menu_item_option}." \n";
-#		}
 	}
 }
 
