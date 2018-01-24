0dbea97a93a30eafd7cf7d0d3407f43bc3a7545a
ME <me@clearbox>
2018-01-24 16:17:24 -0600

    the print function is almost working with width based wrapping.

--- modules/display.pm
+++ modules/display.pm
@@ -1,6 +1,7 @@
 package modules::display;
 use Exporter 'import';
 use Data::Dumper;
+
 &ouut( source => 'display', message => "loading module", tab => 1, severity => 'debug' );
 @EXPORT = qw(ouut ouut_string ouut_menu ouut_line ouut_clear ouut_title ouut_quest);
 $main::data{menu}{main}{display}	=	{
@@ -8,6 +9,7 @@ $main::data{menu}{main}{display}	=	{
 	name => "DISPLAY",
 	description => "Module for configuring display and looking at runtime variables",
 };
+$main::data{config}{screen_width} = 80;
 sub ouut_string	{
 	print shift;
 	print "\n";
@@ -21,6 +23,8 @@ sub ouut	{
 #	* severity: [debugv,debug,warn,error]	this is the severity of the message
 #	* source:	where this message is coming from
 #	* tab: 		this is how many spaces in front for hierarchy
+
+#here is some screen sizing magic
 	my (%params) = @_;
 	my $output_line = "| ";
 	my %san_input =	(
@@ -31,16 +35,13 @@ sub ouut	{
 		source  => ($params{source}||"Anonymous"), 
 		tab  => ($params{tab}||0)
 	);
-	if ($main::debug)	{
-		for ( sort (keys %params))	{
-			print "$_ -> ".$params{$_}."\n";
-		}
-	}
+	my $decorations_size = 4 + $san_input{tab} + 6; # sizing math. 4 = "| " on beginning and "[]" on end plus tabs + 6 which is the spacing incurred by logo + space
 	while ( $san_input{tab} > 0)	{
 		$output_line .= " ";
 		$san_input{tab} --;
 	}
-	$output_line .="[".$san_input{logo}."] - ".time()." - ".$san_input{severity}." - ".$san_input{source}." -> ".$san_input{message} ."\n";
+	my $line = time()." - ".$san_input{severity}." - ".$san_input{source}." -> ".$san_input{message};
+	$output_line .="[".$san_input{logo}."] - ".$line."\n";
 	print $output_line;
 	
 }
@@ -48,18 +49,19 @@ sub ouut	{
 sub ouut_clear	{
 	return system("clear");
 }
+
 sub ouut_title	{	
 	my $screen_width = 78; # this isn't the actual width, just the width that I made in ouut_line
 	my $input = shift;
-	print "input => $input \n";
+	ouut( message => "input => $input", source=>'display::ouut_title', severity => 'debugv');
 	$input =~ s/ /-/g; # replace spaces with -
 	my $length = length($input);
-	print "length = $length\n";
+	ouut( message => "length => $input", source=>'display::ouut_title', severity => 'debugv');
 	my $filler_size = $screen_width - $input;
-	print "screen: $screen_width math ". ($screen_width - $length ). " end \n";
-	print "filler size: $filler_size Length: $length Screen WIdth $screen_width.....\n";
+	ouut( message => "screen: $screen_width math ". ($screen_width - $length ). " end ", source=>'display::ouut_title', severity => 'debugv');
+	ouut( message => "filler size: $filler_size Length: $length Screen WIdth $screen_width.....", source=>'display::ouut_title', severity => 'debugv');
 	my $line = "|-"; # ip put a single hyphen in there so that i have an equal length on both the front and back
-#	$line .= "-" if (($filler_size %% 2 == 1) and ($filler_size --) );
+	$line .= "-" if (($filler_size % 2 ) and ($filler_size --) ); #then i go and check that the length is even and add a space on the left side and subtract 1
 	for (1..($filler_size/2))	{
 		$line .= "-";
 	}
